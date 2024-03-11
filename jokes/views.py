3f639c4e91e332a9781395c84f37615a8ab928f9
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-03-11 16:12:52 +0700

    Try Workflows

--- jokes/views.py
+++ jokes/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from django.contrib.auth import authenticate
 from jokes.models import AppUser, Jokes, Category
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import JokesSerializer, UserSerializer
 import random
